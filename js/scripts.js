79bc56ddadedcfe4c2143b8b0fb2a800a87a9ab6
lindsay culver & alyssa horrocks <student@epicodus.com>
2015-09-10 12:10:18 -0700

    Return roman numerals for any number input up to 3,999

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,70 @@
-function romanI(num) {
-  var numerals = {1:"I", 4:"IV", 5:"V", 9:"IX", 10:"X", 50:"L", 100:"C", 500:"D", 1000:"M"};
-  var ones = num/1;
+function romans(num) {
   var result = [];
 
-  if (num === 4) {
-    result.push(numerals[4])
-  } else if (num === 9) {
-    result.push(numerals[9])
+  while (num >= 1000) {
+    result.push("M");
+    num -= 1000;
+  }
 
-  } else if (num <= 3) {
-    for(var i = 1; i <= ones; i+= 1) {
-      result.push(numerals[1]);
+  while (num >= 500) {
+    if (num >= 900) {
+      result.push("CM");
+      num -= 900;
+    } else {
+      result.push("D");
+      num -= 500;
     }
-    
-  } else if (num < 9) {
-    var remainder = num % 5;
-    result.push(numerals[5]);
+  }
 
-    for(var j = 1; j <= remainder; j+= 1) {
-      result.push(numerals[1]);
-    };
-  };
+  while (num >= 100) {
+    if (num >= 400) {
+      result.push("CD");
+      num -= 400;
+    } else {
+      result.push("C");
+      num -= 100;
+    }
+  }
 
+  while (num >= 50) {
+    if (num >= 90) {
+      result.push("XC");
+      num -= 90;
+    } else {
+      result.push("L");
+      num -= 50;
+    }
+  }
 
-  return result.join("");
-};
+  while (num >= 10) {
+    if (num >= 40) {
+      result.push("XL");
+      num -= 40;
+    } else {
+      result.push("X");
+      num -= 10;
+    }
+  }
+
+  while (num >= 5) {
+    if (num >= 9) {
+      result.push("IX");
+      num -= 9;
+    } else {
+      result.push("V");
+      num -= 5;
+    }
+  }
+
+  while (num >= 1) {
+    if (num >= 4) {
+      result.push("IV");
+      num -= 4;
+    } else {
+      result.push("I");
+      num -= 1;
+    }
+  }
+
+return result.join("");
+}
--- specs/specs.js
+++ specs/specs.js
@@ -1,9 +1,17 @@
-describe('romanI', function() {
-  it('return roman numerals for numbers less than 3', function () {
-    expect(romanI(3)).to.equal('III')
+describe('romans', function() {
+  it('return roman numerals for the number 1000', function () {
+    expect(romans(1000)).to.equal('M')
   });
 
-  it('returns roman numerals for numbers less than 5', function() {
-    expect(romanI(7)).to.equal('VII')
+  it('returns roman numerals for thousands', function() {
+    expect(romans(3000)).to.equal('MMM')
+  });
+
+  it('returns any number that is a basic roman numeral', function() {
+    expect(romans(251)).to.equal("CCLI")
+  });
+
+  it('returns any number that is a special case roman numeral', function() {
+    expect(romans(3454)).to.equal("MMMCDLIV")
   });
 });
